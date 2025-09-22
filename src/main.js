77433706bc2ee28b27810ac61647efc6f8d14350
arthurlanz <arthurlanznaster@gmail.com>
2025-09-22 12:37:46 -0300

    feat: feito o toast #8

--- src/main.js
+++ src/main.js
@@ -6,9 +6,25 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import Toast, { POSITION } from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+  // Configuração global do Toast
+  app.use(Toast, {
+    transition: "Vue-Toastification__bounce",
+    maxToasts: 20,
+    newestOnTop: true,
+    position: POSITION.TOP_RIGHT,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: false,
+    draggable: false,
+    hideProgressBar: true,
+  });
 
 app.mount('#app')
